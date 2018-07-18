319e7c107d261649a8d79ca1f2cc4ae27ae9a774
Matthew <mdwareing@gmail.com>
2018-07-18 11:21:11 +0100

    Add mark method to show index taken

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,12 @@
 class Board
-    attr_reader :size, :spaces
-    def initialize(opts = {})
-        @size = opts[:size]
-        @spaces = spaces
-    end
+  attr_reader :size, :spaces, :mark
+  def initialize(opts = {})
+    @size = opts[:size]
+    @spaces = [nil] * size ** 2
+  end
 
-    def spaces
-        size * size
-    end
+  def mark(index, player)
+    @spaces[index] = player
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,13 +1,26 @@
 require 'board.rb'
 
 describe Board do
-    it 'should return board size' do
-        board = Board.new(size: 3)
-        expect(board.size).to eq 3
-    end
+  it 'should return board size' do
+    board = Board.new(size: 3)
+    expect(board.size).to eq 3
+  end
 
-    it 'should generate a board^2 array' do
-        board = Board.new(size: 3)
-        expect(board.spaces).to eq 9
-    end
+  it 'should generate board^2 array' do
+    board = Board.new(size: 3)
+    expect(board.spaces).to be_a_kind_of(Array)
+    expect(board.spaces.length).to eq 9
+  end
+
+  it 'should be able to mark an index with a X' do
+    board = Board.new(size: 3)
+    board.mark(0, :x)
+    expect(board.spaces[0]).to eq :x
+  end
+
+  it 'should be able to mark an index with a O' do
+    board = Board.new(size: 3)
+    board.mark(4, :O)
+    expect(board.spaces[4]).to eq :O
+  end
 end
