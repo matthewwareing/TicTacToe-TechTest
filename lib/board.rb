cb2e52b3ba5e538e3e36d687c5a9285cf1ce68a8
Matthew <mdwareing@gmail.com>
2018-07-18 10:22:35 +0100

    Initialises a size^2 board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,11 @@
 class Board
-    attr_reader :size
+    attr_reader :size, :spaces
     def initialize(opts = {})
         @size = opts[:size]
+        @spaces = spaces
+    end
+
+    def spaces
+        size * size
     end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,13 @@
 require 'board.rb'
 
 describe Board do
-    it 'should take a board size when initialising' do
+    it 'should return board size' do
         board = Board.new(size: 3)
         expect(board.size).to eq 3
+    end
+
+    it 'should generate a board^2 array' do
+        board = Board.new(size: 3)
+        expect(board.spaces).to eq 9
     end
 end
